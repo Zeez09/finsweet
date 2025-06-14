d378755c718697c1189e74438755df0fa0ca6a84
zeez09 <harunaabdulazeez766@gmail.com>
2025-06-14 14:02:26 +0100

    added hero section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+
+
+
+
+const navLinks = [
+  { label: "About Us", path: "/About Us"},
+  { label: "Careers", path: "/Careers"},
+  { label: "Services", path: "/Services"},
+  { label: "Blog", path: "/Blog"},
+  { label: "Contact Us", path: "/Contact Us"},
+  ];
 
 const NavBar = () => {
   return (
-    <div>
-                  <h3 className='font-bold'>Finsweet</h3>
-    </div>
+    <>
+      <div className='pt-7 w-full'>
+        <nav className='flex items-center justify-between text-black max-w-7xl mx-auto'>
+          
+            <div>
+            <h3 className='font-bold'>Finsweet</h3>
+          </div>
+
+          <div className="flex items-center gap-6">
+
+          <ul className=" gap-6 text-sm text-black flex flex-row justify-center font-bold">
+            {navLinks.map((link, index) => (
+              <li key={index} className="hover:text-black cursor-pointer capitalize">
+                <Link to={link.path}>{link.label}</Link>
+                </li>
+              ))}
+              </ul>
+
+
+          
+
+
+
+          <button className='bg-rose-200 px-4 py-2 rounded-md font-bold'>clone project</button>
+                    </div>
+
+
+
+
+        </nav>
+      </div>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,73 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import logo2 from "../assets/images/Logo (2).svg"
+import logo1 from "../assets/images/Logo (1).svg"
+import logo from "../assets/images/Logo.svg"
+import hero from "../assets/images/aafd339d1834d9e71b4ccb2fe6ff32efd99491a7 (1).png"
+
+
 
 const Home = () => {
   return (
-<div className="border-2 border-red-500 p-4">
 
-  <NavBar/>
+<div className="w-full">
+  
+  <div className="lg:px-8 mb-7 ">
+    
+      <NavBar />
+  </div>
+
+    
+    
+
+  <div className='bg-blue-200 min-h-screen w-full pt-7'>
+    <div className="lg:px-8 max-w-7xl mx-auto flex items-center gap-8">
+
+
+      <div className='flex-1'>
+        <h3 className='text-7xl font-semibold text-black mt-20 leading-tight '>
+        Prosper with Our<br/>bespoke solutions
+      </h3>
+      <p className='text-2xl font-medium mt-2.5 leading-tight'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br/>
+      Suspendisse varius enim in eros elementum tristique</p>
+    
+    <div className='flex flex-row mt-8 gap-6 text-2xl'>
+      <button className='bg-amber-600 px-10 py-3 rounded-4xl text-white'>See Our Services</button>
+      <button>See All Services</button>
+
       
+    </div>
+
+    <p className='mt-20 text-black text-lg'>worked with 100+ companies</p>
+
+    <div className='flex flex-row gap-6'>
+        <img src={logo2} alt="" />
+        <img src={logo1} alt="" />
+        <img src={logo} alt="" />
+        </div>
+        
+      </div>
+
+      <div className='flex-1'>
+        <img src={hero} alt="" className="w-full h-auto object-cover mt-8" />
+      </div>
+      </div>
+  
+      </div>
+
+
+
+      <div className="mt-12 max-w-7xl mx-auto">
+        <h2 className='text-5xl font-semibold leading-[48px]'>
+          We help more than 1500<br/>companies from all sectors
+        </h2>
+        <p className='font-medium text-lg leading-[30px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros<br/>
+        blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et<br/>
+        pellentesque.</p>
+      </div>
+    
+  
+
     </div>
   )
 }
